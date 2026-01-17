9657ae158ab963f57a23041d665385ddebb2acaf
pixelmar <marceloencarnacionpolack@gmail.com>
2026-01-17 22:05:53 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+let particles = [];
+
+function setup() {
+  createCanvas(600, 400);
+  
+}
+
+function draw() {
+  background(0);
+  let p = new Particle();
+  particles.push(p)
+  for (let i = 0; i < particles.length; i++) {
+    particles[i].show()
+    particles[i].update()
+  } 
+}
+
+class Particle {
+  
+  constructor() {
+    this.x = 300;
+    this.y = 380;
+    this.vx = random (-1, 1);
+    this.vy = random (-5, -1);
+  }
+  
+  finished() {
+    pass;
+  }
+  update() {
+    this.x += this.vx;
+    this.y += this.vy;
+  }
+  show() {
+    stroke(255);
+    fill(255, 10);
+    ellipse(this.x, this.y, 16);
+  }
+  
+  
+}
